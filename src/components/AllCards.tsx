eb995775868a7828a822aff36e910864e0776939
gabriele_simone <gabriele.simone1@studenti.unisalento.it>
2022-09-16 16:09:50 +0200

    add: allcards

--- src/components/AllCards.tsx
+++ src/components/AllCards.tsx
@@ -2,6 +2,8 @@ import { Button } from "./utils/Button";
 import Card from "./utils/Card";
 
 export default function AllCards() {
+  console.log(window.innerWidth);
+
   return (
     <>
       <div id="card-section">
@@ -9,7 +11,6 @@ export default function AllCards() {
           <h1 className="title">Giochi in evidenza</h1>
           <div className="sub-container">
             <div className="first-line">
-
               <Card
                 source="https://media.contentapi.ea.com/content/dam/gin/images/2022/07/eas-fifa22-gen5-ue-keyart-horz-f23logo.jpg.adapt.crop1x1.767p.jpg"
                 logo="./assets/cards/fifa-logo.svg"
@@ -18,34 +19,36 @@ export default function AllCards() {
               <Card
                 source="./assets/cards/apex.jpg"
                 logo="./assets/cards/apex-logo.svg"
-                game="fifa"
+                game="apex"
               />
               <Card
-                source="./assets/cards/fifa.jpg"
-                logo="./assets/cards/fifa-logo.svg"
-                game="fifa"
+                source="./assets/cards/battlefield.jpg"
+                logo="./assets/cards/battlefield-logo.svg"
+                game="battle"
               />
-
             </div>
             <div className="second-line">
-            <Card
-                source="https://media.contentapi.ea.com/content/dam/gin/images/2022/07/eas-fifa22-gen5-ue-keyart-horz-f23logo.jpg.adapt.crop1x1.767p.jpg"
-                logo="./assets/cards/fifa-logo.svg"
-                game="fifa"
+              <Card
+                source="./assets/cards/the-sims.jpg"
+                logo="./assets/cards/the-sims-logo.png"
+                game="sims"
               />
               <Card
-                source="./assets/cards/apex.jpg"
-                logo="./assets/cards/apex-logo.svg"
-                game="fifa"
+                source="./assets/cards/car.jpg"
+                logo="./assets/cards/car-logo.png"
+                game="car"
               />
               <Card
-                source="./assets/cards/fifa.jpg"
-                logo="./assets/cards/fifa-logo.svg"
-                game="fifa"
+                source="./assets/cards/it-takes-twop.jpg"
+                logo="./assets/cards/it-takes-two-logo.png"
+                game="two"
               />
             </div>
           </div>
-          <Button buttonSize="btn--medium" buttonStyle="btn--outline-black">
+          <Button
+            buttonSize="btn--medium"
+            buttonStyle="btn--outline-black"
+          >
             Ultimi giochi
           </Button>
         </div>
