6acf65974328c6ea50747fbb57590980d1c1b624
Tiziano Forte <forte.tiziano@gmail.com>
2022-09-21 15:58:15 +0200

    add:slider

--- src/components/utils/Carousel.tsx
+++ src/components/utils/Carousel.tsx
@@ -1,9 +1,75 @@
 import React from 'react'
+import { useState } from 'react';
 
-export default function Carousel() {
+
+const slideStyles = {
+
+};
+
+const rightArrowStyles = {
+  
+};
+
+const leftArrowStyles = {
+  
+};
+
+const sliderStyles = {
+  
+};
+
+const dotsContainerStyles = {
+  
+};
+
+const dotStyle = {
+ 
+};
+
+const ImageSlider = ({ slides}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+  const slideStylesWidthBackground = {
+    ...slideStyles,
+    backgroundImage: `url(${slides[currentIndex].url})`,
+  };
+
   return (
-    <div>
-        
+    <div style={sliderStyles}>
+      <div>
+        <div onClick={goToPrevious} style={leftArrowStyles}>
+          ❰
+        </div>
+        <div onClick={goToNext} style={rightArrowStyles}>
+          ❱
+        </div>
+      </div>
+      <div style={slideStylesWidthBackground}></div>
+      <div style={dotsContainerStyles}>
+        {slides.map((slide, slideIndex) => (
+          <div
+            style={dotStyle}
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+          >
+            
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default ImageSlider;
