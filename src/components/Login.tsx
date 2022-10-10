42aa3cff77951cfb3944099e7eafea0aabb1131c
gabriele_simone <gabriele.simone1@studenti.unisalento.it>
2022-10-10 13:30:20 +0200

    dev: login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,24 +1,68 @@
-
+import { useState } from "react";
 
 export function Login() {
+  const handleSubmit = (event: any) => {
+    // Prevent page reload
+    event.preventDefault();
+  };
 
+  const [values, setValues] = useState("");
+  
+  const handleChangevalues = (value: any) => {
+    setValues((prevValue) => ({
+      //@ts-ignore
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }));
+    console.log(values)
+  };
 
+  const sendToServer = () => {
+    console.log("Inviato!")
+  }
 
-    return (
-        <div className="conteiner-login">
-            <form className="flex-column-center">
-                <div className="input-container flex-column-center">
-                    <h4>Username</h4>
-                    <input type="text" name="uname" required className="input"/>
-                </div>
-                <div className="input-container flex-column-center">
-                    <h4>Password</h4>
-                    <input type="password" name="pass" required className="input"/>
-                </div>
-                <div className="button-container">
-                    <input type="submit" value="Login" className="signin"/>
-                </div>
-            </form>
+  /* const handleLogin=() =>{
+    this.setState({
+        login : true
+    })       
+}
+const handleResetState =() => {
+    this.setState({
+        username: '',
+        password :'',
+        checked : '',
+        login: false ,
+        disabled : ''
+    })
+} */
+
+  return (
+    <div className="conteiner-login">
+      <div className="flex-column-center form" onSubmit={handleSubmit}>
+        <div className="input-container flex-column-center">
+          <h4>Email</h4>
+          <input
+            type="text"
+            name="email"
+            required
+            className="input"
+            onChange={handleChangevalues}
+          />
         </div>
-    )
+        <div className="input-container flex-column-center">
+          <h4>Password</h4>
+          <input
+            type="password"
+            name="pass"
+            required
+            className="input"
+            onChange={handleChangevalues}
+          />
+        </div>
+        <div className="button-container">
+          <input type="submit" value="Login" className="signin" onClick={sendToServer}/>
+        </div>
+      </div>
+    </div>
+  );
 }
