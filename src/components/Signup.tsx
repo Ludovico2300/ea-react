594dca5e106e5df15540227d746a09cfae37f26e
gabriele_simone <gabriele.simone1@studenti.unisalento.it>
2022-10-11 12:54:44 +0200

    fix: nav css

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,5 +1,4 @@
 import { useRef, useState } from "react";
-import { useApi } from "./hooks/useApi";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -8,20 +7,10 @@ export function Singup() {
   const inputRefUser: any = useRef();
   const inputRefEmail: any = useRef();
   const inputRefPass: any = useRef();
-  /*   const [response, setResponse]: any = useState(); */
+  const [response, setResponse]: any = useState();
 
-  const { fetch, res, toast } = useApi({
-    path: "signup",
-    user: {
-      email: inputRefEmail.current.value,
-      password: inputRefPass.current.value,
-      username: inputRefUser.current.value,
-    },
-  });
-
-  console.log(res);
-
-  /*    axios
+  const fetch = () => {
+    axios
       .post(`http://localhost:3030/EA-server/signup`, {
         email: inputRefEmail.current.value,
         password: inputRefPass.current.value,
@@ -53,50 +42,61 @@ export function Singup() {
             theme: "colored",
           });
         }
-      }); */
+      });
+  };
 
   return (
     <div className="conteiner-singup">
       <ToastContainer style={{ fontSize: "1.5rem" }} />
       <div className="form">
-        <div className="input-container ">
-          <h4>Email</h4>
-          <input
-            type="text"
-            name="email"
-            required
-            className="input"
-            ref={inputRefEmail}
-          />
+
+
+        <div className="first">
+
+          <div className="input-container ">
+            <h4>Email</h4>
+            <input
+              type="text"
+              name="email"
+              required
+              className="input"
+              ref={inputRefEmail}
+            />
+          </div>
+          <div className="input-container">
+            <h4>Username</h4>
+            <input
+              type="text"
+              name="user"
+              required
+              className="input"
+              ref={inputRefUser}
+            />
+          </div>
         </div>
-        <div className="input-container">
-          <h4>Username</h4>
-          <input
-            type="text"
-            name="user"
-            required
-            className="input"
-            ref={inputRefUser}
-          />
-        </div>
-        <div className="input-container ">
-          <h4>Password</h4>
-          <input
-            type="password"
-            name="pass"
-            required
-            className="input"
-            ref={inputRefPass}
-          />
-        </div>
-        <div className="input-container ">
-          <h4>Verify(Password)</h4>
-          <input type="password" name="pass" required className="input" />
+
+
+        <div className="second">
+
+          <div className="input-container ">
+            <h4>Password</h4>
+            <input
+              type="password"
+              name="pass"
+              required
+              className="input"
+              ref={inputRefPass}
+            />
+          </div>
+          <div className="input-container ">
+            <h4>Conferm Password</h4>
+            <input type="password" name="pass" required className="input" />
+          </div>
         </div>
 
         <div className="button-container">
           <button value="singup" className="signin" onClick={fetch}>
-            singup{" "}
+            Singup {response ? response.data.status : ""}
           </button>
         </div>
       </div>
